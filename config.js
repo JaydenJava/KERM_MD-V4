81f1610853dd9a904cdbcbe617c5d4a5b9f2029d
Jayden Java <jaydenkmb56@gmail.com>
2025-05-07 13:46:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aBYDFRTY#XsFdDMpHEm2qoetc6IH0HZkW9TaCruabqfTRMOyAJmQ",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+SESSION_ID: process.env.SESSION_ID || "a09b0cceceeaa1c830c72b9e1a24595a",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
